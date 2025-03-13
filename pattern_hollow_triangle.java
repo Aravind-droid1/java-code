21a71553cddff4c76cc5107cb8c7972a82f71a79
Aravind P <aravindshankar486@gmail.com>
2025-03-13 21:35:18 +0530

    Add files via upload

---
+++ pattern_hollow_triangle.java
@@ -0,0 +1,31 @@
+class Demo 
+{
+	public static void main(String[] args) 
+	{
+		
+		int count=1;
+		int n=5;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n-i;j++){
+				System.out.print(" ");
+			}
+			if(count+1 == n*2){
+				for(int r=1;r<= (n*2)-1;r++){
+					System.out.print("*");
+				}
+			}
+			else{
+				for(int s=1;s<= count; s++){
+					if(s==1 || s==count){
+						System.out.print("*");
+					}
+					else{
+						System.out.print(" ");
+					}
+				}
+			count=count+2;
+			System.out.println();
+			}
+		}
+	}
+}
